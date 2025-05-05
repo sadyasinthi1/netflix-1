ea8771a03ee58e1e0039842e77db9afc17173ef4
Sadya Hossain Sinthi <sadyasinthi1@gmail.com>
2025-05-05 13:22:57 -0400

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,21 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwHGZM9wZKgpkKXm9ZLNlfh1HKbMNVSrw",
+  authDomain: "netflix-clone-18f7b.firebaseapp.com",
+  projectId: "netflix-clone-18f7b",
+  storageBucket: "netflix-clone-18f7b.firebasestorage.app",
+  messagingSenderId: "213195906681",
+  appId: "1:213195906681:web:bce44182702d0545573ea8",
+  measurementId: "G-FDMQ9BLCQK"
+};
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmQ5YjMwOWMzYTU5YzJmYmRkZDA5M2U4ZDgyZGEzOSIsIm5iZiI6MTc0NjE3MDU3NC41LCJzdWIiOiI2ODE0NzJjZWE2NWUyOWI1ZWZmNDA2MTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5Gl9gJG6Vfb54Mx5s0S0M3iryqMOQ18PWarl8De-tyc";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
